912e089b3c2a5f615662061a06a1bd30694fd4fb
m0n73 <m0nt3@protonmail.com>
2019-09-19 06:49:26 -0400

    inital commit

---
+++ rms-quote.el
@@ -0,0 +1,75 @@
+;;;  rms-quote.el  --- get a juicy quote from RMS. -*- lexical-binding: t; -*-
+
+;;  Copyright (C) 2019 by m0nt3
+
+;;  Author:     m0nt3
+;;  Created     19 Sep 2019
+;;  Name:       rms-quote
+;;  Version:    0.1.2
+;;  Keywords:   RMS Stallman wisdom sex parrot
+;;  License:    GPL v3
+;;  Package-Requires: ((emacs "24.1"))
+
+;;  This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;;  This packages introduces a bit of wisdom into
+;;  your workflow: a random quote by our beloved
+;;  RMS on sensitive subjects. Don't see a quote
+;;  you love? No problem -- adding a quote is as
+;;  easy as "C-h C-y".
+;;  NOTE: This package binds two functions to 
+;;  keys. Modify those lines if these bindings
+;;  clash with your current setup.
+
+;;;  Install:
+
+;;  Place rms-quote.el and rms-quotes.list
+;;  in "~/.emacs.d/rms-quote/".
+;;
+;;  Add the following to your emacs init:
+;;  (add-to-list 'load-path "~/.emacs.d/rms-quote")
+;;  (require 'rms-quote)
+;;
+;;  "C-h C-y" to add current line to the quotes.
+;;  "C-h C-u" to enjoy yet another pearl from RMS. 
+;;
+
+(random t)
+(setq quotes-list-file 
+      "~/.emacs.d/rms-quote/rms-quotes.list")
+
+(defun read-rms-quotes(path)
+  "Read quotes from 'path' into a list"
+  (with-temp-buffer 
+    (insert-file-contents path)
+    (split-string (buffer-string) "\n" t)))
+
+(defun rms-qstr-add(path qstr)
+  "Save 'qstr' into a file 'path'"
+  (when (y-or-n-p "Add current line to RMS quotes?")
+    (write-region qstr nil path t)))
+
+(setq rms-quotes-list 
+      (read-rms-quotes quotes-list-file))
+
+(defun rms-quote()
+  "Enjoy another fantastic quote by RMS"
+  (interactive)
+  (message (nth 
+             (random 
+             (length rms-quotes-list)) 
+            rms-quotes-list)))
+
+(defun rms-quote-add()
+  "Add the current line to the quotes file."
+  (interactive)
+  (rms-qstr-add quotes-list-file 
+               (thing-at-point 'line t)))
+
+;; Change the keybindings here.
+(global-set-key (kbd "C-h C-u") 'rms-quote)
+(global-set-key (kbd "C-h C-y") 'rms-quote-add)
+
+(provide 'rms-quote)
